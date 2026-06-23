82d4a35166acd5ce6fe8a1cda5c9fc6ae19a9792
laura-jung <166522604+laura-jung@users.noreply.github.com>
2026-06-23 19:05:32 +0900

    init: first commit

---
+++ src/test/java/com/Timo/Timo/TimoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Timo.Timo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
