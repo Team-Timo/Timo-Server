951d057cd6fd7ea83caf199ee29e27c694afb167
aneykrap <aneykrap@gmail.com>
2026-06-27 17:59:38 +0900

    feat: Swagger api 문서 설정 추가

---
+++ src/main/java/com/Timo/Timo/global/config/SwaggerConfig.java
@@ -0,0 +1,18 @@
+package com.Timo.Timo.global.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+	@Bean
+	public OpenAPI customOpenAPI() {
+		return new OpenAPI()
+				.info(new Info()
+						.title("Timo API")
+                        .description("Timo 서버 API 명세서")
+						.version("v1"));
+	}
+}
