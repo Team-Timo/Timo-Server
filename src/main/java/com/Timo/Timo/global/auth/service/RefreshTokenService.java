1f44da32ba6500533a856f9d3c7e020a9b5a4ab7
jy000n <jayoonchang@naver.com>
2026-06-30 13:20:17 +0900

    fix: Redis TTL 단위를 JWT refresh token 만료 시간 단위(s)와 일치하도록 수정

--- src/main/java/com/Timo/Timo/global/auth/service/RefreshTokenService.java
+++ src/main/java/com/Timo/Timo/global/auth/service/RefreshTokenService.java
@@ -20,7 +20,7 @@ public class RefreshTokenService {
         KEY_PREFIX + email,
         refreshToken,
         jwtTokenProvider.getRefreshTokenExpiry(),
-        TimeUnit.MILLISECONDS
+        TimeUnit.SECONDS
     );
   }
 
