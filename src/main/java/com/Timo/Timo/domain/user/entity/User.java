d97344d42c6fbd3fff4e8686787f4e0f6dbb5870
jy000n <jayoonchang@naver.com>
2026-07-02 11:29:09 +0900

    refactor: User 생성자를 private Builder로 제한

--- src/main/java/com/Timo/Timo/domain/user/entity/User.java
+++ src/main/java/com/Timo/Timo/domain/user/entity/User.java
@@ -1,5 +1,6 @@
 package com.Timo.Timo.domain.user.entity;
 
+import com.Timo.Timo.domain.user.enums.Provider;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -9,7 +10,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,8 +18,6 @@ import lombok.NoArgsConstructor;
 @Table(name = "users")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Builder
-@AllArgsConstructor
 public class User {
 
   @Id
@@ -36,10 +34,17 @@ public class User {
   @Column(nullable = false)
   private Provider provider;
 
+  @Builder
+  private User(String email, String name, String imageUrl, Provider provider) {
+    this.email = email;
+    this.name = name;
+    this.imageUrl = imageUrl;
+    this.provider = provider;
+  }
+
   public void update(String name, String imageUrl){
     this.name = name;
     this.imageUrl = imageUrl;
   }
 
-  public enum Provider { GOOGLE }
 }
