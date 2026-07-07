9563d91ac051b225d5a5ce45900f7690e6d11938
jy000n <jayoonchang@naver.com>
2026-07-07 22:50:40 +0900

    feat: timer 엔티티 추가

--- src/main/java/com/Timo/Timo/domain/todo/repository/TodoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.Timo.Timo.domain.todo.repository;
-
-import com.Timo.Timo.domain.todo.entity.Todo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TodoRepository extends JpaRepository<Todo, Long> {
-}
