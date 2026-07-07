98fb50f9c8bdda52d99cf0bea2735d12506174b5
jy000n <jayoonchang@naver.com>
2026-07-07 23:14:33 +0900

    refactor: 타이머 SuccessCode enum에 BaseSuccessCode 적용

--- src/main/java/com/Timo/Timo/domain/timer/exception/TimerSuccessCode.java
+++ src/main/java/com/Timo/Timo/domain/timer/exception/TimerSuccessCode.java
@@ -1,12 +1,13 @@
 package com.Timo.Timo.domain.timer.exception;
 
+import com.Timo.Timo.global.exception.code.BaseSuccessCode;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 
 @Getter
 @RequiredArgsConstructor
-public enum TimerSuccessCode {
+public enum TimerSuccessCode implements BaseSuccessCode {
 
   TIMER_STARTED(HttpStatus.CREATED, "TIMER_201", "타이머가 시작되었습니다."),
   TIMER_PAUSED(HttpStatus.OK, "TIMER_200", "타이머가 일시정지되었습니다."),
