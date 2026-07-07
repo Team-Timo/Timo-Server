456ef9e156f262ebdb786424904b4acd089986b9
jy000n <jayoonchang@naver.com>
2026-07-07 16:19:20 +0900

    feat: 타이머 errorCode, successCode 추가

---
+++ src/main/java/com/Timo/Timo/domain/timer/exception/TimerSuccessCode.java
@@ -0,0 +1,18 @@
+package com.Timo.Timo.domain.timer.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum TimerSuccessCode {
+
+  TIMER_STARTED(HttpStatus.CREATED, "TIMER_201", "타이머가 시작되었습니다."),
+  TIMER_PAUSED(HttpStatus.OK, "TIMER_200", "타이머가 일시정지되었습니다."),
+  TIMER_RESUMED(HttpStatus.OK, "TIMER_200", "타이머가 재개되었습니다.");
+
+  private final HttpStatus httpStatus;
+  private final String code;
+  private final String message;
+}
