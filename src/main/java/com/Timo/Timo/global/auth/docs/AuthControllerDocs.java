65f92fbe2512a9527d5338346744be4e97a32804
jy000n <jayoonchang@naver.com>
2026-07-07 18:15:59 +0900

    chore: 불필요한 import 제거

--- src/main/java/com/Timo/Timo/global/auth/docs/AuthControllerDocs.java
+++ src/main/java/com/Timo/Timo/global/auth/docs/AuthControllerDocs.java
@@ -15,7 +15,6 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.servlet.http.HttpServletRequest;
-import java.util.Map;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestBody;
