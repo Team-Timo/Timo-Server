d327301e4df53180a4b26f1dfc8c2279bdf011ac
aneykrap <aneykrap@gmail.com>
2026-07-07 14:23:42 +0900

    fix: 충돌 해결

--- src/main/java/com/Timo/Timo/domain/user/controller/UserController.java
+++ src/main/java/com/Timo/Timo/domain/user/controller/UserController.java
@@ -1,15 +1,18 @@
 package com.Timo.Timo.domain.user.controller;
 
+import com.Timo.Timo.domain.user.docs.UserLanguageDocs;
+import com.Timo.Timo.domain.user.docs.UserProfileDocs;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.Timo.Timo.domain.user.docs.UserControllerDocs;
 import com.Timo.Timo.domain.user.dto.response.UserProfileResponse;
 import com.Timo.Timo.domain.user.exception.UserSuccessCode;
 import com.Timo.Timo.domain.user.service.UserService;
+import com.Timo.Timo.global.exception.CustomException;
+import com.Timo.Timo.global.exception.code.ErrorCode;
 import com.Timo.Timo.global.auth.principal.CustomUserDetails;
 import com.Timo.Timo.global.response.BaseResponse;
 
@@ -20,7 +23,7 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/api/v1/users")
 @RequiredArgsConstructor
 @Tag(name = "User", description = "사용자 API")
-public class UserController implements UserControllerDocs {
+public class UserController implements UserProfileDocs, UserLanguageDocs {
 
 	private final UserService userService;
 
