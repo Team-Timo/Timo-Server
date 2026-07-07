4d68f287cd574876ec24ae67f29ca908abe11b54
jy000n <jayoonchang@naver.com>
2026-07-07 20:05:23 +0900

    fix(todo): Todo 엔티티에 타이머에 필요한 임시 User 연관관계 추가

--- src/main/java/com/Timo/Timo/domain/todo/entity/Todo.java
+++ src/main/java/com/Timo/Timo/domain/todo/entity/Todo.java
@@ -1,10 +1,14 @@
 package com.Timo.Timo.domain.todo.entity;
 
+import com.Timo.Timo.domain.user.entity.User;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -20,6 +24,10 @@ public class Todo {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   @Column(name = "todo_id")
   private Long id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "user_id", nullable = false)
+  private User user;
 
   @Column(name = "estimated_minutes")
   private Integer estimatedMinutes;
