1c72586d422a971a2cf00bc852882a53cb466302
jy000n <jayoonchang@naver.com>
2026-07-07 15:58:27 +0900

    feat: timerRecord, timerSession 레포지토리 추가

---
+++ src/main/java/com/Timo/Timo/domain/timer/repository/TimerRecordRepository.java
@@ -0,0 +1,12 @@
+package com.Timo.Timo.domain.timer.repository;
+
+import com.Timo.Timo.domain.timer.entity.TimerRecord;
+import com.Timo.Timo.domain.timer.enums.TimerStatus;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TimerRecordRepository extends JpaRepository<TimerRecord, Long> {
+
+    Optional<TimerRecord> findByUserIdAndStatusIn(Long userId, List<TimerStatus> statuses);
+}
