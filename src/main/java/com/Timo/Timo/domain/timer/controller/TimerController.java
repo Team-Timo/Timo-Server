6f98a21dba06bd77360f2c3a48dc7b114ea9dab0
jy000n <jayoonchang@naver.com>
2026-07-07 18:10:40 +0900

    feat: 타이머 시작 컨트롤러 구현

---
+++ src/main/java/com/Timo/Timo/domain/timer/controller/TimerController.java
@@ -0,0 +1,40 @@
+package com.Timo.Timo.domain.timer.controller;
+
+import com.Timo.Timo.domain.timer.docs.TimerControllerDocs;
+import com.Timo.Timo.domain.timer.dto.response.TimerStartResponse;
+import com.Timo.Timo.domain.timer.exception.TimerSuccessCode;
+import com.Timo.Timo.domain.timer.service.TimerService;
+import com.Timo.Timo.global.auth.principal.CustomUserDetails;
+import com.Timo.Timo.global.response.BaseResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "Timer", description = "타이머 API")
+@RestController
+@RequestMapping("/api/v1")
+@RequiredArgsConstructor
+public class TimerController implements TimerControllerDocs {
+
+  private final TimerService timerService;
+
+  @Override
+  @PostMapping("/todos/{todoId}/timers/start")
+  public ResponseEntity<BaseResponse<TimerStartResponse>> startTimer(
+      @PathVariable Long todoId,
+      @AuthenticationPrincipal CustomUserDetails userDetails
+  ) {
+
+    Long userId = userDetails.getUser().getId();
+    TimerStartResponse response = timerService.startTimer(userId, todoId);
+
+    return ResponseEntity.status(HttpStatus.CREATED)
+        .body(BaseResponse.onSuccess(TimerSuccessCode.TIMER_STARTED, response));
+  }
+}
