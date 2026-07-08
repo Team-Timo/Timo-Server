1421bf7208252afcfebd74ef7b653e437349820e
jy000n <jayoonchang@naver.com>
2026-07-09 01:36:51 +0900

    feat: 타이머 일시정지/재개 API 엔드포인트 추가

--- src/main/java/com/Timo/Timo/domain/timer/controller/TimerStartController.java
+++ src/main/java/com/Timo/Timo/domain/timer/controller/TimerStartController.java
@@ -1,17 +1,25 @@
 package com.Timo.Timo.domain.timer.controller;
 
-import com.Timo.Timo.domain.timer.docs.TimerControllerDocs;
+import com.Timo.Timo.domain.timer.docs.TimerStartControllerDocs;
+import com.Timo.Timo.domain.timer.docs.TimerStatusControllerDocs;
+import com.Timo.Timo.domain.timer.dto.request.TimerActionRequest;
 import com.Timo.Timo.domain.timer.dto.response.TimerStartResponse;
+import com.Timo.Timo.domain.timer.dto.response.TimerStatusResponse;
+import com.Timo.Timo.domain.timer.enums.TimerAction;
+import com.Timo.Timo.domain.timer.exception.TimerSuccessCode;
 import com.Timo.Timo.domain.timer.factory.TimerResponseFactory;
 import com.Timo.Timo.domain.timer.service.TimerService;
 import com.Timo.Timo.global.auth.principal.CustomUserDetails;
 import com.Timo.Timo.global.response.BaseResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,7 +27,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1")
 @RequiredArgsConstructor
-public class TimerController implements TimerControllerDocs {
+public class TimerStartController implements TimerStartControllerDocs, TimerStatusControllerDocs {
 
   private final TimerService timerService;
   private final TimerResponseFactory timerResponseFactory;
@@ -35,5 +43,22 @@ public class TimerController implements TimerControllerDocs {
     TimerStartResponse response = timerService.startTimer(userId, todoId);
 
     return timerResponseFactory.startResponse(response);
+  }
+
+  @Override
+  @PatchMapping("timers/{timerId}/status")
+  public ResponseEntity<BaseResponse<TimerStatusResponse>> changeStatus(
+    @PathVariable Long timerId,
+    @Valid @RequestBody TimerActionRequest request,
+    @AuthenticationPrincipal CustomUserDetails userDetails
+  ){
+    Long userId = userDetails.getUserId();
+    TimerStatusResponse response = timerService.changeStatus(userId, timerId, request.action());
+
+    TimerSuccessCode successCode = request.action() == TimerAction.PAUSE
+        ? TimerSuccessCode.TIMER_PAUSED
+        : TimerSuccessCode.TIMER_RESUMED;
+
+    return timerResponseFactory.statusResponse(response, successCode);
   }
 }
