86a158117ad3cec74bedbaab4d2ffe37281defab
jy000n <jayoonchang@naver.com>
2026-07-09 01:37:03 +0900

    docs: 타이머 일시정지/재개 API Swagger 문서화

--- src/main/java/com/Timo/Timo/domain/timer/docs/TimerStartControllerDocs.java
+++ src/main/java/com/Timo/Timo/domain/timer/docs/TimerStartControllerDocs.java
@@ -13,7 +13,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 
-public interface TimerControllerDocs {
+public interface TimerStartControllerDocs {
 
   @Operation(
       summary = "타이머 시작",
