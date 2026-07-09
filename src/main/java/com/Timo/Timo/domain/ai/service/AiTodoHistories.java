d346ba28032051cce59671a9a6107edc5bdcd1a6
aneykrap <aneykrap@gmail.com>
2026-07-09 14:32:51 +0900

    chore: 공백 및 여백 제거

--- src/main/java/com/Timo/Timo/domain/ai/service/AiTodoHistories.java
+++ src/main/java/com/Timo/Timo/domain/ai/service/AiTodoHistories.java
@@ -7,5 +7,4 @@ import com.Timo.Timo.domain.ai.dto.TodoDurationHistory;
 public record AiTodoHistories(
 	List<TodoDurationHistory> similarTitleHistories,
 	List<TodoDurationHistory> recentTagHistories
-) {
-}
+) {}
