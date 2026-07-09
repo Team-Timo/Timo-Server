78cbd7456770e6dc92c74afccb4375abc650387c
laura-jung <166522604+laura-jung@users.noreply.github.com>
2026-07-09 19:30:01 +0900

    feat(subtask): 하위 태스크 완료 여부 관련 응답 코드

--- src/main/java/com/Timo/Timo/domain/todo/exception/TodoSuccessCode.java
+++ src/main/java/com/Timo/Timo/domain/todo/exception/TodoSuccessCode.java
@@ -12,7 +12,9 @@ import lombok.RequiredArgsConstructor;
 public enum TodoSuccessCode implements BaseSuccessCode {
 
 	CREATED(HttpStatus.CREATED, "TODO가 생성되었습니다."),
-	STATUS_CHANGED(HttpStatus.OK, "TODO 상태가 변경되었습니다.");
+	STATUS_CHANGED(HttpStatus.OK, "TODO 상태가 변경되었습니다."),
+	SUBTASK_STATUS_CHANGED(HttpStatus.OK, "하위 태스크 상태가 변경되었습니다."),
+	;
 
 	private final HttpStatus httpStatus;
 	private final String message;
