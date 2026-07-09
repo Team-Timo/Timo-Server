a64656c74e763b9f2cbfc4ecaf460971f7365ae2
laura-jung <166522604+laura-jung@users.noreply.github.com>
2026-07-09 19:29:51 +0900

    feat(subtask): 하위 태스크 완료 여부 변경

--- src/main/java/com/Timo/Timo/domain/todo/entity/Subtask.java
+++ src/main/java/com/Timo/Timo/domain/todo/entity/Subtask.java
@@ -50,4 +50,8 @@ public class Subtask extends BaseTimeEntity {
 	void assignTodo(Todo todo) {
 		this.todo = todo;
 	}
+
+	public void updateCompleted(boolean completed) {
+		this.completed = completed;
+	}
 }
