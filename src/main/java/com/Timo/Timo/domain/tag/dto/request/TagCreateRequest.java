3b4ae0a5035eec16fa9fb554698ad9aa9f50d66a
laura-jung <166522604+laura-jung@users.noreply.github.com>
2026-07-09 22:21:58 +0900

    refactor(tag): 태그 이름 제약 조건 수정

--- src/main/java/com/Timo/Timo/domain/tag/dto/request/TagCreateRequest.java
+++ src/main/java/com/Timo/Timo/domain/tag/dto/request/TagCreateRequest.java
@@ -5,6 +5,6 @@ import jakarta.validation.constraints.Size;
 
 public record TagCreateRequest(
 		@NotBlank(message = "태그 이름은 필수입니다.")
-		@Size(max = 20, message = "태그 이름은 20자를 초과할 수 없습니다.")
+		@Size(max = 10, message = "태그 이름은 10자를 초과할 수 없습니다.")
 		String name
 ) { }
