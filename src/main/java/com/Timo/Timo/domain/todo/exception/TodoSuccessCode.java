03564bc38aa2a229dabe190af4991e4f0c58824f
laura-jung <166522604+laura-jung@users.noreply.github.com>
2026-07-09 18:41:01 +0900

    feat(todo): todo 순서 정렬 응답 코드

--- src/main/java/com/Timo/Timo/domain/todo/exception/TodoSuccessCode.java
+++ src/main/java/com/Timo/Timo/domain/todo/exception/TodoSuccessCode.java
@@ -14,6 +14,7 @@ public enum TodoSuccessCode implements BaseSuccessCode {
 	CREATED(HttpStatus.CREATED, "TODO가 생성되었습니다."),
 	GET_DETAIL(HttpStatus.OK, "TODO 상세 조회 성공"),
 	STATUS_CHANGED(HttpStatus.OK, "TODO 상태가 변경되었습니다."),
+	REORDERED(HttpStatus.OK, "TODO 순서가 변경되었습니다."),
 	;
 
 	private final HttpStatus httpStatus;
