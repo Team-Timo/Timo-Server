408de2ea441bcd4518294d4ba10b2bf77bfab61f
jy000n <jayoonchang@naver.com>
2026-07-11 01:22:47 +0900

    refactor: 불필요한 import, 로직 제거

--- src/main/java/com/Timo/Timo/domain/user/entity/User.java
+++ src/main/java/com/Timo/Timo/domain/user/entity/User.java
@@ -2,9 +2,7 @@ package com.Timo.Timo.domain.user.entity;
 
 import com.Timo.Timo.domain.user.enums.Language;
 import com.Timo.Timo.domain.user.enums.Provider;
-import com.Timo.Timo.domain.user.exception.UserErrorCode;
 import com.Timo.Timo.global.common.BaseTimeEntity;
-import com.Timo.Timo.global.exception.CustomException;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -90,15 +88,11 @@ public class User extends BaseTimeEntity {
       LocalTime wakeUpTime,
       LocalTime bedTime
   ) {
-
-    if (!termsAgreed) {
-      throw new CustomException(UserErrorCode.TERMS_AGREEMENT_REQUIRED);
-    }
-
     this.language = language;
     this.predictionAccuracy = predictionAccuracy;
     this.wakeUpTime = wakeUpTime;
     this.bedTime = bedTime;
+    this.termsAgreed = true;
     this.onboardingCompleted = true;
   }
 
