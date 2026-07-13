7f1de4b171068e66159373a5a172115447c721c5
aneykrap <aneykrap@gmail.com>
2026-07-14 01:16:27 +0900

    feat: Terms by type and language 추가

--- src/main/java/com/Timo/Timo/domain/terms/repository/TermsRepository.java
+++ src/main/java/com/Timo/Timo/domain/terms/repository/TermsRepository.java
@@ -1,7 +1,13 @@
 package com.Timo.Timo.domain.terms.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.Timo.Timo.domain.terms.entity.Terms;
+import com.Timo.Timo.domain.terms.enums.TermsLanguage;
+import com.Timo.Timo.domain.terms.enums.TermsType;
 
-public interface TermsRepository extends JpaRepository<Terms, Long> {}
+public interface TermsRepository extends JpaRepository<Terms, Long> {
+	Optional<Terms> findFirstByTypeAndLanguageOrderByIdDesc(TermsType type, TermsLanguage language);
+}
--- src/main/java/com/Timo/Timo/domain/terms/service/TermsService.java
+++ src/main/java/com/Timo/Timo/domain/terms/service/TermsService.java
@@ -5,6 +5,8 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.Timo.Timo.domain.terms.dto.response.TermsDetailResponse;
 import com.Timo.Timo.domain.terms.entity.Terms;
+import com.Timo.Timo.domain.terms.enums.TermsLanguage;
+import com.Timo.Timo.domain.terms.enums.TermsType;
 import com.Timo.Timo.domain.terms.repository.TermsRepository;
 import com.Timo.Timo.global.exception.CustomException;
 import com.Timo.Timo.global.exception.code.ErrorCode;
@@ -17,8 +19,11 @@ import lombok.RequiredArgsConstructor;
 public class TermsService {
 	private final TermsRepository termsRepository;
 
-	public TermsDetailResponse getTerms(Long termsId) {
-		Terms terms = termsRepository.findById(termsId)
+	public TermsDetailResponse getTermsByCondition(String type, String language) {
+		Terms terms = termsRepository.findFirstByTypeAndLanguageOrderByIdDesc(
+				TermsType.from(type),
+				TermsLanguage.from(language)
+			)
 			.orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND));
 
 		return new TermsDetailResponse(
