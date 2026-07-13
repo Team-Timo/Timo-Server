ed7c978411a9f618fdd949fd2c60067468c4edad
jy000n <jayoonchang@naver.com>
2026-07-14 03:04:06 +0900

    fix: UserErrorCode에 TERMS_AGREEMENT_REQUIRED 누락된 코드 추가

--- src/main/java/com/Timo/Timo/domain/user/exception/UserErrorCode.java
+++ src/main/java/com/Timo/Timo/domain/user/exception/UserErrorCode.java
@@ -11,6 +11,7 @@ public enum UserErrorCode implements BaseErrorCode {
 
   USER_INVALID_TIME(HttpStatus.BAD_REQUEST, "USER_400", "취침 시간은 기상 시간보다 이후여야 합니다."),
   INVALID_TIMEZONE(HttpStatus.BAD_REQUEST, "USER_400", "유효하지 않은 시간대 ID입니다."),
+  TERMS_AGREEMENT_REQUIRED(HttpStatus.BAD_REQUEST, "USER_400", "약관 동의가 필요합니다."),
   USER_NOT_FOUND(HttpStatus.NOT_FOUND, "USER_404", "존재하지 않는 사용자입니다."),
   ;
 
