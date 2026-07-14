62fac13f289c5d6e389c50fbf574292234479eb3
jy000n <jayoonchang@naver.com>
2026-07-15 01:59:09 +0900

    feat: 구글 이벤트 날짜 파싱 유틸 추가

---
+++ src/main/java/com/Timo/Timo/domain/calendar/utils/GoogleEventDateParser.java
@@ -0,0 +1,37 @@
+package com.Timo.Timo.domain.calendar.utils;
+
+import com.Timo.Timo.domain.calendar.dto.GoogleEventDateTime;
+import java.time.LocalDate;
+import java.time.OffsetDateTime;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class GoogleEventDateParser {
+
+  public static LocalDate parseStart(GoogleEventDateTime start) {
+    if (start == null) {
+      return null;
+    }
+    if (start.date() != null) {
+      return LocalDate.parse(start.date());
+    }
+    if (start.dateTime() != null) {
+      return OffsetDateTime.parse(start.dateTime()).toLocalDate();
+    }
+    return null;
+  }
+
+  public static LocalDate parseEnd(GoogleEventDateTime end) {
+    if (end == null) {
+      return null;
+    }
+    if (end.date() != null) {
+      return LocalDate.parse(end.date()).minusDays(1);
+    }
+    if (end.dateTime() != null) {
+      return OffsetDateTime.parse(end.dateTime()).toLocalDate();
+    }
+    return null;
+  }
+}
