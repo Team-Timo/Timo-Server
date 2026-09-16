6db2be7089e1323a62564960d54df2b648524391
aneykrap <aneykrap@gmail.com>
2026-09-16 12:26:44 +0900

    fix(Statistics): 예정일과 다른 날짜에 실행된 타이머 기록 일별 통계 누락 수정

--- src/main/java/com/Timo/Timo/domain/statistics/service/StatisticsService.java
+++ src/main/java/com/Timo/Timo/domain/statistics/service/StatisticsService.java
@@ -6,10 +6,12 @@ import java.time.YearMonth;
 import java.time.ZoneId;
 import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -138,8 +140,9 @@ public class StatisticsService {
 			.collect(Collectors.toMap(TimerDailyTodoStats::getTodoId, TimerDailyTodoStats::getActualSeconds));
 
 		List<Todo> occurringTodos = statisticsOccurrenceCalculator.findOccurringRules(userId, date);
-		Map<Long, String> tagNamesById = findTagNames(occurringTodos);
-		List<DailyTodoResponse> todos = occurringTodos.stream()
+		List<Todo> dailyTodos = mergeWithRecordedTodos(occurringTodos, actualSecondsByTodoId.keySet());
+		Map<Long, String> tagNamesById = findTagNames(dailyTodos);
+		List<DailyTodoResponse> todos = dailyTodos.stream()
 			.map(todo -> toDailyTodoResponse(todo, actualSecondsByTodoId, tagNamesById))
 			.toList();
 
@@ -166,6 +169,20 @@ public class StatisticsService {
 				.toLocalDate())
 			.distinct()
 			.count();
+	}
+
+	private List<Todo> mergeWithRecordedTodos(List<Todo> occurringTodos, Set<Long> recordedTodoIds) {
+		Set<Long> occurringTodoIds = occurringTodos.stream().map(Todo::getId).collect(Collectors.toSet());
+		List<Long> missingTodoIds = recordedTodoIds.stream()
+			.filter(todoId -> !occurringTodoIds.contains(todoId))
+			.toList();
+		if (missingTodoIds.isEmpty()) {
+			return occurringTodos;
+		}
+
+		List<Todo> mergedTodos = new ArrayList<>(occurringTodos);
+		mergedTodos.addAll(statisticsOccurrenceCalculator.findRulesByIds(missingTodoIds));
+		return mergedTodos;
 	}
 
 	private int calculateCompletionRate(DailyOccurrence occurrence) {
